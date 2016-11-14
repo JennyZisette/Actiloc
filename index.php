be0e8473c83667defdb31567a1a7fd385eb7ae64
Laura Glanzmann <Laura@ext-133-229.eduroam.unibe.ch>
2016-11-14 16:34:00 +0100

    css und index apassig

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
-indexlaura
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>ActiLoc</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/actiloc.css" rel="stylesheet" type="text/css">
+
+    <!-- jQuery code-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+    <!-- fürs Plugin -->
+    <link rel="stylesheet" href="plugin/css/ap-scroll-top.css">
+    <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="plugin/js/ap-scroll-top.js"></script>
+  </head>
+  <body>
+        <div class="page-header col-xs-12 header">
+          <header>
+            <a href="index.php">
+            <h1>ActiLoc </h1>
+            </a>
+          </header>
+      </div>
+      <!-- Formular für Angebotssuche-->
+      <div>
+        <div class="col-xs-12">
+          <h2> Wohin willst du?</h2>
+
+          <form method="post">
+            <div class="form-group">
+                 <div class="form-group">
+                      <label for="ortsfeld"></label>
+                      <input type="text" class="form-control" id="ortsfeld" name="ortsfeld" placeholder="Wohin willst du?" maxlength="30" required="required">
+                 </div>
+            </div>
+                 <button type="submit" class="btn btn-default">Restaurant</button>
+                 <button type="submit" class="btn btn-default">Aktivitäten</button>
+          </form>
+
+        </div>
+      </div>
+  </body>
+<html>

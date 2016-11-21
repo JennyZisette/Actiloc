8dcd1dc39082198474586a21e64a2db3f642bea1
Laura Glanzmann <Laura@Lauras-MacBook-Pro-2.local>
2016-11-21 11:21:38 +0100

    button

--- restaurant.php
+++ restaurant.php
@@ -43,8 +43,8 @@ else{
       </div>
       <?php   while($restaurant = mysqli_fetch_assoc($restaurant_list)) {  ?>
 
-        <div class="row"><!-- Beitrag-->
-            <div class="col-xs-10">
+        <div class="row"><!-- Restaurant Listenelement-->
+            <div class="col-xs-8">
               <div class="panel panel-default">
                 <div class="panel-heading">
                   <h3 class="panel-title"><?php echo $restaurant['name']; ?></h3>
@@ -53,9 +53,11 @@ else{
                   <p><?php echo $restaurant['adresse'] . " " . $restaurant['ort_id'] . " " . $restaurant['ortsname']; ?></p> <br>
                   <p><?php echo $restaurant['lead'];?> </p>
               </div>
-            </div>
-        </div> <!-- /Beitrag -->
+              <div class="panel-footer">
+                  <button type="submit" class="btn btn-default"> Mehr Infos </button>
+              </div>
+        </div> <!-- /Restaurant Listenelement -->
       </div>
- <?php   } ?>
+      <?php   } ?>
 
    </body>
